44e38f03087887fd58f3d4e12f64cadb62eecf74
samuelmahjouri <samuel.mahjouri@gmail.com>
2021-08-31 10:07:18 -0700

    little git proj with weight from 8/31

--- src/main/java/com/example/sping_portfolio/controllers/test.java
+++ src/main/java/com/example/sping_portfolio/controllers/test.java
@@ -2,7 +2,33 @@ package com.example.sping_portfolio.controllers;
 
 public class test {
     public static void main(String[] args) {
-        double
+        double scoreOne;
+        scoreOne = 2;
+
+        double scoreTwo;
+        scoreTwo = 4;
+
+        double scoreThree;
+        scoreThree = 6;
+
+        double scoreFour;
+        scoreFour = 8;
+
+        double scoreFive;
+        scoreFive = 10;
+
+        double averageoftwo;
+        averageoftwo = (scoreOne + scoreTwo)/2;
+
+        double averageoffour;
+        averageoffour = (scoreThree + scoreFour)/2;
+
+        double averageofall;
+        averageofall = (2*averageoftwo + 2*averageoffour + scoreFive)/5;
+
+        System.out.print(averageofall);
+
+
     }
 
 
