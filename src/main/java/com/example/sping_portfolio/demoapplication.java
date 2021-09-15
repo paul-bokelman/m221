e7aa30349fc10b862bc4a4b5da1027a039673977
samuelmahjouri <samuel.mahjouri@gmail.com>
2021-09-15 08:56:03 -0700

    finalized the demoapp file

--- src/main/java/com/example/sping_portfolio/demoapplication.java
+++ src/main/java/com/example/sping_portfolio/demoapplication.java
@@ -25,10 +25,8 @@ public class demoapplication {
                 new Student(
                         1L,
                         "Mariam",
-                        "mariam@gasljdfgmail.com"
-                        LocalDate.of(year:2000, )
+                        "mariam@gasljdfgmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
                 )
-                "Hello", "World");
-
-    }
-}
+        );
