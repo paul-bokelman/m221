75cfefc60bf8709eeb05e485a9deb9e92e5f13a5
Paul Bokelman <paul.bokelman1@gmail.com>
2021-09-20 09:11:58 -0700

    LITERALYL DID THE BINARY THING

--- src/main/java/com/example/sping_portfolio/controllers/Binary.java
+++ src/main/java/com/example/sping_portfolio/controllers/Binary.java
@@ -1,15 +1,17 @@
 package com.example.sping_portfolio.controllers;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Binary {
     @GetMapping("/binary")
-    public String page(Model model) {
-        model.addAttribute("binary", "bits");
-        return "binary";
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String binary(@RequestParam(name="bits", required=false, defaultValue="8") int bits, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("bits", bits); // MODEL is passed to html
+        return "binary"; // returns HTML VIEW (greeting)
     }
 }
-
