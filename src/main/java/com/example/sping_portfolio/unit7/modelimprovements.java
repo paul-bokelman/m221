6e0fa8328d49bb885ffbcd9df3b33320454ce8c0
samuelmahjouri <samuel.mahjouri@gmail.com>
2021-10-06 09:15:22 -0700

    images for imageinfo and unit 7 check

---
+++ src/main/java/com/example/sping_portfolio/unit7/modelimprovements.java
@@ -0,0 +1,20 @@
+package com.example.sping_portfolio.unit7;
+
+public class modelimprovements {
+    String prefix = "../../../resources/static/images/";
+    String[] arr = {"image1","image2","image3","image4"};
+
+    public class image_info {
+
+    }
+
+    public class image_property {
+
+    }
+
+    public class image_property_rgb {
+
+    }
+}
+
+
