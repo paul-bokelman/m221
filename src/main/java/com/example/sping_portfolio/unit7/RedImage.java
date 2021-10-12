e1f7bfa94aab681a048e7eb26ab1eb104620bb2c
lucap2527 <tech4days21@gmail.com>
2021-10-12 07:09:36 -0700

    Three commits before are leaderboard creation

---
+++ src/main/java/com/example/sping_portfolio/unit7/RedImage.java
@@ -0,0 +1,55 @@
+package com.example.sping_portfolio.unit7;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class RedImage
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedImage img = null;
+        File f = null;
+
+        // read image
+        try
+        {
+            f = new File("C:\\Users\\pinto\\IdeaProjects\\m221\\src\\main\\resources\\static\\images\\ishaan.jpg");
+            img = ImageIO.read(f);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int p = img.getRGB(x,y);
+
+                int a = (p>>24)&0xff;
+                int r = (p>>16)&0xff;
+
+                p = (a<<24) | (r<<16) | (0<<8) | 0;
+
+                img.setRGB(x, y, p);
+            }
+        }
+
+        try
+        {
+            f = new File("G:\\Out.jpg");
+            ImageIO.write(img, "jpg", f);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
