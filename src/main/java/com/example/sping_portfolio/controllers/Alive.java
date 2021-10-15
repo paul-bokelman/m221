cc62b9a6865317309e6585acf0ac4d99aba7be78
Paul Bokelman <paul.bokelman1@gmail.com>
2021-10-15 08:42:08 -0700

    gray

--- src/main/java/com/example/sping_portfolio/controllers/Alive.java
+++ src/main/java/com/example/sping_portfolio/controllers/Alive.java
@@ -15,23 +15,24 @@ public class Alive {
     public String page(Model model) {
         String[] names = {"Ishan", "Justin", "Luca", "Mort MORT MORT!", "Paul", "Rohan", "Roop", "Ryan", "Sammy", "Yeonjoon"};
         String[] arr = {"images/ishaanreal.png","images/justinli.png","images/lucapinto.png","images/mort.png","images/paulbok.png","images/rohankos.png","images/roopreal.png","images/rynamog.png","images/sammahj.png","images/yeongjoong.png"};
-        String[] dead = {"Ishan", "Luca"}; // any names in this list that match those in the names[] array will be grayscaled with sam's file
+        boolean[] dead = {true, false, true, false, false, false, false, false, false, false}; // any names in this list that match those in the names[] array will be grayscaled with sam's file
 
         // checks for any strings in the dead[] array that match the names[] array and applies the grayscale function to their pictures in arr[]
-        for (int i =0; i < names.length; i++) {
-            for (String k : dead) {
-                if (names[i] == k) {
-                    File input = new File(arr[i]);
-                    String outputName = arr[i].substring(arr[i].indexOf("/") + 1);
-                    System.out.println(outputName);
-                    GrayScale.GrayScale(input, outputName);
-                }
-            }
-        }
+        // for (int i = 0; i < names.length; i++) {
+        //     for (String k : dead) {
+        //         if (names[i] == k) {
+        //             File input = new File(arr[i]);
+        //             String outputName = arr[i].substring(arr[i].indexOf("/") + 1);
+        //             System.out.println(outputName);
+        //             GrayScale.GrayScale(input, outputName);
+        //         }
+        //     }
+        // }
 
         // returns the names and pictures to the HTML view
         model.addAttribute("arr", arr);
         model.addAttribute("names", names);
+        model.addAttribute("dead", dead);
         return "alive"; // returns HTML VIEW (greeting)
     }
 }
