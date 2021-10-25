b3285a5865fa9715f85688786d560ebe6de7ec7c
Paul Bokelman <paul.bokelman1@gmail.com>
2021-10-24 22:37:04 -0700

    changes

--- src/main/java/com/example/sping_portfolio/PaulLab.java
+++ src/main/java/com/example/sping_portfolio/PaulLab.java
@@ -5,7 +5,7 @@ public class PaulLab {
         int multiplyThree(int n1, int n2, int n3);
      }
      
-     public class Demo implements Multiply{
+     public class Methods implements Multiply{
         public int multiplyTwo(int num1, int num2){
            return num1*num2;
         }
@@ -13,10 +13,10 @@ public class PaulLab {
            return num1*num2*num3;
         }
         public int[] main(){
-           Multiply obj = new Demo();
+           Multiply obj = new Methods();
             int two = obj.multiplyTwo(2,3);
             int three = obj.multiplyThree(1, 9, 4);
-            int[] result={two, three};  //initializing array  
+            int[] result={two, three};
            return result;
         }
      }
