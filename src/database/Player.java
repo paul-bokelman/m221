40e9f2060adbf744460d30583efcc04070c4b3a9
iBraskyy <72888867+iBraskyy@users.noreply.github.com>
2021-11-03 21:55:04 -0700

    Update Player.java

--- src/database/Player.java
+++ src/database/Player.java
@@ -1,19 +1,11 @@
 package com.example.sping_portfolio.database;
 
 
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
-@Setter
-@Getter
-@ToString
 public class Player {
 
     @Id
@@ -21,17 +13,14 @@ public class Player {
     private long id;
 
     // Name of the player
-    @NonNull
     @Size(min=2, max=100, message = "Name")
     private String name;
 
     // Where the players image is located so we can display it easily
-    @NonNull
     @Size(min=2, max=300, message="ImgURL")
     private String imgURL;
 
     // Look at PlayerState enum for corresponding states (alive, dead, out)
-    @NonNull
     private int state;
 
     // One to many relationship to hold all the people player killed
